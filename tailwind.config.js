c0cf90346dc9d71f7e058aeaefac3e0410f46367
riezlittoraldig-wq <riezlittoral.dig@gmail.com>
2026-05-04 22:07:30 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -98,5 +98,16 @@ module.exports = {
     		}
     	}
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+      require("tailwindcss-animate"),
+      function({ addUtilities }) {
+        addUtilities({
+          '.scrollbar-hide': {
+            '-ms-overflow-style': 'none',
+            'scrollbar-width': 'none',
+            '&::-webkit-scrollbar': { display: 'none' }
+          }
+        })
+      }
+    ],
   }
